409ecbb79bb7b3d9fb645d2ceecb36e18dec5d26
tupoy-ya <tupoy-ya@users.noreply.github.com>
2025-10-17 22:55:46 +0500

    refactor: Use batch pattern scanning.

--- source/main.cpp
+++ source/main.cpp
@@ -2,6 +2,7 @@
 #include "GarrysMod/Symbol.hpp"
 #include "GarrysMod/InterfacePointers.hpp"
 #include "GarrysMod/FunctionPointers.hpp"
+#include "memory/batch.hpp"
 #include "scanning/symbolfinder.hpp"
 
 // garrysmod_common has pattern scanning, but it's so fucking unreadable and not documented that I'm just gonna take the YimMenu classes instead.
@@ -9,34 +10,57 @@
 #include "memory/module.hpp"
 #include "memory/byte_patch.hpp"
 
-#include "detouring/helpers.hpp"
-#include "detouring/detours.h"
+namespace pointers {
+	static void* allow_invalid_ticket{};
+	static void* allow_wrong_game{};
+
+
+
+	void init_engine()
+	{
+		memory::batch batch;
+
+		batch.add("Steam Auth", "48 8B 95 60 FF FF FF 44", [](memory::handle ptr) {
+			allow_invalid_ticket = ptr.add(46).as<void*>();
+			allow_wrong_game = ptr.sub(27).as<void*>();
+		});
+
+		batch.run(memory::module("engine"));
+	}
+}
 
 namespace byte_patches {
 	static memory::byte_patch* allow_invalid_ticket = nullptr;
 	static memory::byte_patch* allow_wrong_game = nullptr;
+
+	void init_patches()
+	{
+		allow_invalid_ticket = memory::byte_patch::make(pointers::allow_invalid_ticket, {0xE9, 0x16, 0x00, 0x00, 0x00}).get();
+		allow_wrong_game = memory::byte_patch::make(pointers::allow_wrong_game, {0xE9, 0x5F, 0x00, 0x00, 0x00}).get();
+	}
+
+	void apply_patches()
+	{
+		allow_invalid_ticket->apply();
+		allow_wrong_game->apply();
+	}
 }
 
 GMOD_MODULE_OPEN()
 {
-	auto addr = ::memory::module("engine").scan(::memory::pattern("48 8B 95 60 FF FF FF 44")).value();
+	pointers::init_engine();
 
-	byte_patches::allow_invalid_ticket = memory::byte_patch::make(addr.add(46).as<void*>(), {0xE9, 0x16, 0x00, 0x00, 0x00}).get();
-	byte_patches::allow_wrong_game = memory::byte_patch::make(addr.sub(27).as<void*>(), {0xE9, 0x5F, 0x00, 0x00, 0x00}).get();
+	byte_patches::init_patches();
+	byte_patches::apply_patches();
 
-	byte_patches::allow_invalid_ticket->apply();
-	byte_patches::allow_wrong_game->apply();
-	Msg("Allow invalid ticket patch applied.\n");
+	Msg("Remove restrictions patches applied.\n");
 
 	return 0;
 }
 
 GMOD_MODULE_CLOSE()
 {
-	if (byte_patches::allow_invalid_ticket)
-		byte_patches::allow_invalid_ticket->restore();
-	if (byte_patches::allow_wrong_game)
-		byte_patches::allow_wrong_game->restore();
+	memory::byte_patch::restore_all();
 
 	return 0;
 }
